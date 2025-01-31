2bd57b3853ae11bde386460719d817c9dc4e1618
Léo Baccialone <baccialone.leo@gmail.com>
2025-01-31 12:38:57 +0100

    add Google Analytics

--- client/src/pages/_app.page.js
+++ client/src/pages/_app.page.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 // STYLES
 import "@/styles/style.scss";
@@ -9,10 +10,8 @@ import "@/styles/custom/_index.scss";
 // I18N
 import { appWithTranslation } from "next-i18next";
 
-
 // CALENDAR
 import { getCalApi } from "@calcom/embed-react";
-
 
 // SMOOTH SCROLL
 import Lenis from "@studio-freight/lenis";
@@ -58,15 +57,53 @@ function App({ Component, pageProps }) {
       window.scrollTo(0, 0);
     }
   }, [router.asPath]);
-  useEffect(()=>{
-	  (async function () {
-		const cal = await getCalApi({"namespace":"appel-decouverte-30-min"});
-		cal("ui", {"cssVarsPerTheme":{"light":{"cal-brand":"#59DC6C"},"dark":{"cal-brand":"#59DC6C"}},"hideEventTypeDetails":false,"layout":"month_view"});
-	  })();
-	}, [])
-  
 
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "appel-decouverte-30-min" });
+      cal("ui", {
+        cssVarsPerTheme: {
+          light: { "cal-brand": "#59DC6C" },
+          dark: { "cal-brand": "#59DC6C" },
+        },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-X99NL4SYCL", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-X99NL4SYCL"
+        strategy="afterInteractive"
+      />
+
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X99NL4SYCL');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(App);
